fbc3f47700b0cd2dde4f0b27e8acef8cb5003a15
m.kahtooei <m.kahtooei@gmail.com>
2023-02-19 08:57:05 +0330

    add dependencies and resources

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cab_driver/shared/utils/page_routes.dart';
 import 'package:cab_driver/ui/screens/main_screen/main_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Regular-Font',
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen(),
+      initialRoute: PagesRouteData.mainPage,
+      routes: {
+        PagesRouteData.mainPage: (context) => MainScreen(),
+      },
     );
   }
 }
---
+++ lib/shared/utils/page_routes.dart
@@ -0,0 +1,6 @@
+class PagesRouteData {
+  static String loginPage = "Login";
+  static const registerPage = "Register";
+  static const mainPage = "Main";
+  static const searchPage = "SearchPage";
+}
